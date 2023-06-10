ff31bb2d8a197a83497167c46d256f68c895ac62
Muhammad Rafi <88550428+RaxonRafi@users.noreply.github.com>
2023-06-10 20:47:17 +0600

    Update

--- src/controllers/localityController.js
+++ src/controllers/localityController.js
@@ -42,13 +42,10 @@ exports.create=(request,response)=>{
    const category = request.body['category'];
    const subcategory = request.body['subcategory'];
    const discount_code = request.body['discount_code'];
-   const latitude = request.body['latitude'];
-   const longitude = request.body['longitude'];
    const accepts_credit_cards = request.body['accepts_credit_cards'];
    const is_owner_verified = request.body['is_owner_verified'];
    const start_date = request.body['start_date'];
    const end_date = request.body['end_date'];
-   const language = request.body['language'];
    const status = request.body['status'];
    const social_networks = request.body['social_networks'];
    const startDate = new Date(start_date);
@@ -59,9 +56,8 @@ exports.create=(request,response)=>{
    const now = new Date();
    const created_at = now.toISOString().slice(0, 19).replace('T', ' ');
 
-//    const sql = `INSERT INTO negocio (business_id, schedule, name, price, manager, description, address_1, address_2, address_3, city, state, country, postal_code,delivery, shipping, bill, antiquity, email, physical_store, online_store, url_google,business_age,municipality,cell_phone_number, business_days, category, subcategory, discount_code, publication_likes, questions, policies_terms, accepts_credit_cards, is_owner_verified,social_networks, status, created_at, start_date, end_date) VALUES ('${business_id}','${schedule}','${name}','${price}','${manager}','${description}','${address_1}','${address_2}','${address_3}','${city}','${state}','${country}','${postal_code}','${delivery}','${shipping}','${bill}','${antiquity}','${email}','${physical_store}','${online_store}','${url_google}','${business_age}','${municipality},'${cell_phone_number}','${business_days}','${category}','${subcategory}','${discount_code}','${publication_likes}','${questions}','${policies_terms}','${accepts_credit_cards}','${is_owner_verified}','${social_networks}','${status}','${created_at}','${start_date}','${end_date}')`;
 
-const sql = `INSERT INTO negocio(business_id, schedule, name, price, manager, description, address_1, address_2, address_3, city, state, country, postal_code, delivery, shipping, bill, antiquity, email, physical_store, online_store, url_google, business_age, municipality, cell_phone_number, business_days, category, subcategory, discount_code, publication_likes, questions,policies_terms, accepts_credit_cards, is_owner_verified, social_networks, status, start_date, end_date) VALUES ("${business_id}",'${schedule}','${name}','${price}','${manager}','${description}','${address_1}','${address_2}','${address_3}','${city}','${state}','${country}','${postal_code}','${delivery}','${shipping}','${bill}','${antiquity}','${email}','${physical_store}','${online_store}','${url_google}','${business_age}','${municipality}','${cell_phone_number}','${business_days}','${category}','${subcategory}','${discount_code}','${publication_likes}','${questions}','${policies_terms}','${accepts_credit_cards}','${is_owner_verified}','${social_networks}','${status}','${start_date}','${end_date}')`
+const sql = `INSERT INTO negocio(id_business, schedule, name, price, manager, description, address_1, address_2, address_3, city, state, country, postal_code, delivery, shipping, bill, antiquity, email, physical_store, online_store, url_google, business_age, municipality, cell_phone_number, business_days, category, subcategory, discount_code, publication_likes, questions,policies_terms, accepts_credit_cards, is_owner_verified, social_networks, status, start_date, end_date) VALUES ("${business_id}",'${schedule}','${name}','${price}','${manager}','${description}','${address_1}','${address_2}','${address_3}','${city}','${state}','${country}','${postal_code}','${delivery}','${shipping}','${bill}','${antiquity}','${email}','${physical_store}','${online_store}','${url_google}','${business_age}','${municipality}','${cell_phone_number}','${business_days}','${category}','${subcategory}','${discount_code}','${publication_likes}','${questions}','${policies_terms}','${accepts_credit_cards}','${is_owner_verified}','${social_networks}','${status}','${start_date}','${end_date}')`
 
    db.query(sql, (err,data)=>{
 
@@ -96,7 +92,7 @@ const sql = `INSERT INTO negocio(business_id, schedule, name, price, manager, de
 
                     const imgLocation = result.Location;
                   
-                    const ImageSql = `INSERT INTO images(images_id, image_url) VALUES ("${business_id}","${imgLocation}")`;
+                    const ImageSql = `INSERT INTO images(id_business, image_url) VALUES ("${business_id}","${imgLocation}")`;
                     db.query(ImageSql,(err,data)=>{
 
                         console.log(err);
@@ -119,7 +115,7 @@ const sql = `INSERT INTO negocio(business_id, schedule, name, price, manager, de
  
               const logo_url = result.Location
 
-              const logoSql = `INSERT INTO logos(logo_id,logo_url) VALUES ('${business_id}','${logo_url }')`
+              const logoSql = `INSERT INTO logos(id_business,logo_url) VALUES ('${business_id}','${logo_url }')`
 
               db.query(logoSql,(err,logo)=>{
 
@@ -152,8 +148,8 @@ exports.getItems=(req,res)=>{
 
     const business_id = req.params.id;
 
-    const sql = `SELECT * FROM negocio WHERE business_id = "${business_id}"`
-    const img_sql = `SELECT * FROM images WHERE images_id = "${business_id}"`
+    const sql = `SELECT * FROM negocio JOIN logos ON negocio.id_business = logos.id_business WHERE negocio.id_business = "${business_id}"`
+    const img_sql = `SELECT * FROM images WHERE id_business = "${business_id}"`
 
     db.query(sql,(err,data)=>{
 
@@ -203,104 +199,126 @@ exports.getItems=(req,res)=>{
 
 exports.UpdateData=(req,res)=>{
     
-   const business_id = req.body['business_id'];
-   const name = req.body['name'];
-   const price = req.body['price'];
-   const manager = req.body['manager'];
-   const description = req.body['description'];
-   const capacity_people = req.body['capacity_people'];
-   const address_1 = req.body['address_1'];
-   const address_2 = req.body['address_2'];
-   const address_3 = req.body['address_3'];
-   const city = req.body['city'];
-   const state = req.body['state'];
-   const country = req.body['country'];
-   const postal_code = req.body['postal_code'];
-   const email = req.body['email'];
-   const cell_phone_number = req.body['cell_phone_number'];
-   const publication_likes = req.body['publication_likes'];
-   const questions = req.body['questions'];
-   const policies_terms = req.body['policies_terms'];
-   const delivery = req.body['delivery'];
-   const shipping = req.body['shipping'];
-   const bill  = req.body['bill'];
-   const antiquity = req.body['antiquity'];
-   const physical_store = req.body['physical_store'];
-   const online_store = req.body['online_store'];
-   const url_google = req.body['url_google'];
-   const business_days = req.body['business_days'];
-   const category = req.body['category'];
-   const subcategory = req.body['subcategory'];
-   const discount_code = req.body['discount_code'];
-   const latitude = req.body['latitude'];
-   const longitude = req.body['longitude'];
-   const accepts_credit_cards = req.body['accepts_credit_cards'];
-   const is_owner_verified = req.body['is_owner_verified'];
-   const start_date = req.body['start_date'];
-   const end_date = req.body['end_date'];
-   const language = req.body['language'];
-   const status = req.body['status'];
-   const social_networks = req.body['social_networks'];
-   const startDate = new Date(start_date);
-   const endDate = new Date(end_date);
-   const startTime = startDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true });
-   const endTime = endDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true });
-   const schedule = startTime+"-"+endTime;
-   const created_at = new Date();
+    const business_id = req.body['business_id'];
+    const selectSql = `SELECT * FROM negocio WHERE id_business = '${business_id}'`;
 
+    db.query(selectSql,(err,data)=>{
 
-const UpdateSql = `UPDATE negocio SET 
-                            name='${name}',
-                            price='${price}',
-                            manager='${manager}',
-                            description='${description}',
-                            address_1='${address_1}',
-                            address_2='${address_2}',
-                            address_3='${address_3}',
-                            city='${city}',
-                            state='${state}',
-                            country='${country}',
-                            postal_code='${postal_code}',
-                            latitude='${latitude}',
-                            longitude='${longitude}',
-                            delivery='${delivery}',
-                            shipping='${shipping}',
-                            bill='${bill}',
-                            antiquity='${antiquity}',
-                            email='${email}',
-                            physical_store='${physical_store}',
-                            online_store='${online_store}',
-                            url_google='${url_google}',
-                            cell_phone_number='${cell_phone_number}',
-                            business_days='${business_days}',
-                            category='${category}',
-                            subcategory='${subcategory}',
-                            discount_code='${discount_code}',
-                            publication_likes='${publication_likes}',
-                            questions='${questions}',
-                            policies_terms='${policies_terms}',
-                            accepts_credit_cards='${accepts_credit_cards}',
-                            is_owner_verified='${is_owner_verified}',
-                            language='${language}',
-                            social_networks='${social_networks}',
-                            status='${status}',
-                            start_date='${start_date}',
-                            end_date='${end_date}'
-                   WHERE business_id = '${business_id}'`;
+        if(err){
 
-   db.query(UpdateSql,(err,data)=>{
+            res.status(500).json({ message: "Error occurred while checking ID existence", error: err });
 
-       if(err){
-          
-        res.status(500).json({message:"data Update failed",error:err})
+        }else{
 
-       }else{
+            if(data.length === 0){
 
-        res.status(200).json({message:"data Updated Successfully",error:data})
+                res.status(404).json({ message: "ID not found" });
 
-       }
+            }else{
 
-   })
+               
+                const name = req.body['name'];
+                const price = req.body['price'];
+                const manager = req.body['manager'];
+                const description = req.body['description'];
+                const capacity_people = req.body['capacity_people'];
+                const address_1 = req.body['address_1'];
+                const address_2 = req.body['address_2'];
+                const address_3 = req.body['address_3'];
+                const city = req.body['city'];
+                const state = req.body['state'];
+                const country = req.body['country'];
+                const postal_code = req.body['postal_code'];
+                const email = req.body['email'];
+                const cell_phone_number = req.body['cell_phone_number'];
+                const publication_likes = req.body['publication_likes'];
+                const questions = req.body['questions'];
+                const policies_terms = req.body['policies_terms'];
+                const delivery = req.body['delivery'];
+                const shipping = req.body['shipping'];
+                const bill  = req.body['bill'];
+                const antiquity = req.body['antiquity'];
+                const physical_store = req.body['physical_store'];
+                const online_store = req.body['online_store'];
+                const url_google = req.body['url_google'];
+                const business_days = req.body['business_days'];
+                const category = req.body['category'];
+                const subcategory = req.body['subcategory'];
+                const discount_code = req.body['discount_code'];
+                const business_age = req.body['business_age'];
+                const municipality = req.body['municipality'];
+                const accepts_credit_cards = req.body['accepts_credit_cards'];
+                const is_owner_verified = req.body['is_owner_verified'];
+                const start_date = req.body['start_date'];
+                const end_date = req.body['end_date'];
+                const status = req.body['status'];
+                const social_networks = req.body['social_networks'];
+                const startDate = new Date(start_date);
+                const endDate = new Date(end_date);
+                const startTime = startDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true });
+                const endTime = endDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true });
+                const schedule = startTime+"-"+endTime;
+                const created_at = new Date();
+             
+             
+             const UpdateSql = `UPDATE negocio SET 
+                                         name='${name}',
+                                         price='${price}',
+                                         manager='${manager}',
+                                         description='${description}',
+                                         address_1='${address_1}',
+                                         address_2='${address_2}',
+                                         address_3='${address_3}',
+                                         city='${city}',
+                                         state='${state}',
+                                         country='${country}',
+                                         postal_code='${postal_code}',
+                                         delivery='${delivery}',
+                                         shipping='${shipping}',
+                                         bill='${bill}',
+                                         antiquity='${antiquity}',
+                                         email='${email}',
+                                         physical_store='${physical_store}',
+                                         online_store='${online_store}',
+                                         url_google='${url_google}',
+                                         business_age ='${business_age}',
+                                         municipality='${municipality}',
+                                         cell_phone_number='${cell_phone_number}',
+                                         business_days='${business_days}',
+                                         category='${category}',
+                                         subcategory='${subcategory}',
+                                         discount_code='${discount_code}',
+                                         publication_likes='${publication_likes}',
+                                         questions='${questions}',
+                                         policies_terms='${policies_terms}',
+                                         accepts_credit_cards='${accepts_credit_cards}',
+                                         is_owner_verified='${is_owner_verified}',
+                                         social_networks='${social_networks}',
+                                         status='${status}',
+                                         start_date='${start_date}',
+                                         end_date='${end_date}'
+                                WHERE id_business = '${business_id}'`;
+             
+                db.query(UpdateSql,(err,data)=>{
+             
+                    if(err){
+                       
+                     res.status(500).json({message:"data Update failed",error:err})
+             
+                    }else{
+             
+                     res.status(200).json({message:"data Updated Successfully",error:data})
+             
+                    }
+             
+                })
+
+            }
+
+        }
+    })
+    
+
 }
 
 
@@ -308,9 +326,9 @@ exports.deleteItem=(req,res)=>{
 
     const business_id = req.params.id;
 
-    const deleteSql = `DELETE FROM negocio WHERE business_id = '${business_id}'`
-    const ImgDelSql = `DELETE FROM images WHERE images_id ='${business_id}'`
-    const imgSelect_sql = `SELECT * FROM images WHERE images_id = "${business_id}"`
+    const deleteSql = `DELETE FROM negocio WHERE id_business = '${business_id}'`
+    const ImgDelSql = `DELETE FROM images WHERE id_business ='${business_id}'`
+    const imgSelect_sql = `SELECT * FROM images WHERE id_business = "${business_id}"`
 
              db.query(imgSelect_sql,(err,images)=>{
 
